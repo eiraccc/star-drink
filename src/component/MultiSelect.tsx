8fd81ba139040c2fd9f1bfde330a1b612652cb6f
Eira <jie44011@gmail.com>
2025-07-27 21:28:07 +0100

    feat: show check icon for selected options in MultiSelect component

--- src/component/MultiSelect.tsx
+++ src/component/MultiSelect.tsx
@@ -1,17 +1,18 @@
-import { OptionType } from '../constants/drink'
-import Select, { components } from 'react-select';
-import CreatableSelect from 'react-select/creatable';
-import { tagColorMap } from '../constants/drink';
+import {OptionType} from '../constants/drink'
+import Select, {components} from 'react-select'
+import CreatableSelect from 'react-select/creatable'
+import {tagColorMap} from '../constants/drink'
+import {FaCheck} from 'react-icons/fa6'
 
 type MultiSelectProps<T extends OptionType> = {
-  options: T[];
-  selected: T[];
-  setSelected: (val: T[]) => void;
-  placeholder?: string;
-  maxToShow?: number;
-  creatable?: boolean;
-  onCreateOption?: (input: string) => void;
-};
+  options: T[]
+  selected: T[]
+  setSelected: (val: T[]) => void
+  placeholder?: string
+  maxToShow?: number
+  creatable?: boolean
+  onCreateOption?: (input: string) => void
+}
 
 const MultiSelect = <T extends OptionType>({
   options,
@@ -20,109 +21,124 @@ const MultiSelect = <T extends OptionType>({
   placeholder = 'Select options',
   maxToShow = 2,
   creatable = false,
-  onCreateOption
+  onCreateOption,
 }: MultiSelectProps<T>) => {
-      const MultiValue = (props: any) => {
-        const { index, getValue, data } = props;
-        const selectedValues = getValue();
-    
-        if (index < maxToShow) {
-          // 顯示前幾個選項的標籤
-          return <components.MultiValue {...props} />;
-        }
-    
-        if (index === maxToShow) {
-          // 超過的顯示 "+N more" 標籤
-          const moreCount = selectedValues.length - maxToShow;
-          return (
-            <div
-              style={{
-                fontSize: '0.75rem',
-                color: '#666',
-                marginLeft: 4,
-                cursor: 'default',
-                userSelect: 'none',
-              }}
-              title={selectedValues.slice(maxToShow).map((v: any) => v.label).join(', ')}
-            >
-              +{moreCount} more
-            </div>
-          );
-        }
-    
-        // 其他多餘的標籤不渲染
-        return null;
-      };
-    
-      const customStyles = {
-        // option
-        option: (base: any, { data, isFocused, isSelected }: {
-          data: OptionType,
-          isFocused: boolean,
-          isSelected: boolean
-        }) => {
-          const tagColor = tagColorMap[data.type];
-          const bgColor = tagColor && data.opacity !== undefined 
-              ? tagColor.bg(data.opacity / 100) 
-              : 'var(--color-surface)';
-          const textColor = tagColor ? tagColor.text : 'var(--color-text)';
-          
-          return {
-            ...base,
-            backgroundColor: isFocused ? bgColor : 'transparent',
-            color: textColor,
-            cursor: 'pointer',
-            fontWeight: isSelected ? 'bold' : 'normal',
-          };
-        },
-        // selected item tag
-        multiValue: (base: any, { data }: { data: OptionType }) => {
-          const tagColor = tagColorMap[data.type];
-          const bgColor = tagColor && data.opacity !== undefined 
-              ? tagColor.bg(data.opacity / 100) 
-              : 'var(--color-surface)';
-          const textColor = tagColor ? tagColor.text : 'var(--color-text)';
-
-          return {
-            ...base,
-            backgroundColor: bgColor,
-            borderRadius: 9999,
-            fontSize: '1rem',
-            padding: '1px 4px',
-            color: textColor,
-          };
-        },
-        // selected item label
-        multiValueLabel: (base: any) => ({
-          ...base,
-          color: 'inherit',
-        }),
-        // selected item remove btn
-        multiValueRemove: (base: any) => ({
-          ...base,
-          ':hover': {
-            color: 'var(--color-secondary)',
-          },
-        }),
-      };
-
-      const SelectComponent = creatable ? CreatableSelect : Select;
-
+  const Option = (props: any) => {
     return (
-        <SelectComponent
-            isMulti
-            options={options}
-            value={selected}
-            onChange={(newVal) => setSelected([...newVal])}
-            placeholder={placeholder}
-            styles={customStyles}
-            components={{ MultiValue }}
-            hideSelectedOptions={false}
-            closeMenuOnSelect={false}
-            className="w-full text-m"
-            onCreateOption={creatable ? onCreateOption : undefined}
-        />
+      <components.Option {...props}>
+        <div className="flex justify-between items-center">
+          <span>{props.data.label}</span>
+          {props.isSelected && <FaCheck />}
+        </div>
+      </components.Option>
     )
+  }
+  const MultiValue = (props: any) => {
+    const {index, getValue, data} = props
+    const selectedValues = getValue()
+
+    if (index < maxToShow) {
+      // 顯示前幾個選項的標籤
+      return <components.MultiValue {...props} />
+    }
+
+    if (index === maxToShow) {
+      // 超過的顯示 "+N more" 標籤
+      const moreCount = selectedValues.length - maxToShow
+      return (
+        <div
+          className="text-xs text-text-secondary ml-1 cursor-default select-none"
+          title={selectedValues
+            .slice(maxToShow)
+            .map((v: any) => v.label)
+            .join(', ')}>
+          +{moreCount} more
+        </div>
+      )
+    }
+
+    // 其他多餘的標籤不渲染
+    return null
+  }
+
+  const customStyles = {
+    // option
+    option: (
+      base: any,
+      {
+        data,
+        isFocused,
+        isSelected,
+      }: {
+        data: OptionType
+        isFocused: boolean
+        isSelected: boolean
+      }
+    ) => {
+      const tagColor = tagColorMap[data.type]
+      const bgColor =
+        tagColor && data.opacity !== undefined
+          ? tagColor.bg(data.opacity / 100)
+          : 'var(--color-surface)'
+      const textColor = tagColor ? tagColor.text : 'var(--color-text)'
+
+      return {
+        ...base,
+        backgroundColor: isFocused ? bgColor : 'transparent',
+        color: textColor,
+        cursor: 'pointer',
+        fontWeight: isSelected ? 'bold' : 'normal',
+      }
+    },
+    // selected item tag
+    multiValue: (base: any, {data}: {data: OptionType}) => {
+      const tagColor = tagColorMap[data.type]
+      const bgColor =
+        tagColor && data.opacity !== undefined
+          ? tagColor.bg(data.opacity / 100)
+          : 'var(--color-surface)'
+      const textColor = tagColor ? tagColor.text : 'var(--color-text)'
+
+      return {
+        ...base,
+        backgroundColor: bgColor,
+        borderRadius: 9999,
+        fontSize: '1rem',
+        padding: '1px 4px',
+        color: textColor,
+      }
+    },
+    // selected item label
+    multiValueLabel: (base: any) => ({
+      ...base,
+      color: 'inherit',
+    }),
+    // selected item remove btn
+    multiValueRemove: (base: any) => ({
+      ...base,
+      ':hover': {
+        color: 'var(--color-secondary)',
+      },
+    }),
+  }
+
+  const SelectComponent = creatable ? CreatableSelect : Select
+
+  return (
+    <SelectComponent
+      isMulti
+      options={options}
+      value={selected}
+      onChange={newVal => setSelected([...newVal])}
+      placeholder={placeholder}
+      styles={customStyles}
+      components={{MultiValue, Option}}
+      hideSelectedOptions={false}
+      closeMenuOnSelect={false}
+      className="w-full text-m"
+      onCreateOption={creatable ? onCreateOption : undefined}
+    />
+  )
 }
 
 export default MultiSelect

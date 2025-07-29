2ec35db37c979a2c6e66a31655b03aaae94cf7b3
Eira <jie44011@gmail.com>
2025-07-29 21:45:16 +0100

    feat: add collapsible filter menu for mobile view

--- src/component/FilterBar.tsx
+++ src/component/FilterBar.tsx
@@ -13,8 +13,11 @@ import {
   RiArrowUpSLine,
   RiSearchLine,
   RiTimeLine,
+  RiFilterLine,
+  RiArrowUpSFill,
 } from 'react-icons/ri';
 import { FaRegStar } from 'react-icons/fa';
+import { useState } from 'react';
 
 type propsType = {
   searchValue: string;
@@ -41,21 +44,44 @@ const FilterBar = ({
   sort,
   toggleSort,
 }: propsType) => {
+  const [isMobileFilterOpen, setIsMobileFilterOpen] = useState<boolean>(false);
+
   return (
-    <div className="flex flex-col gap-4 p-3 mb-3">
-      {/* input */}
-      <div className="flex items-center gap-2 border-2 border-surface focus-within:border-primary rounded-full px-4 py-2 w-full">
-        <RiSearchLine className="text-text font-bold" />
-        <input
-          value={searchValue}
-          onChange={e => setSearchValue(e.target.value)}
-          type="text"
-          placeholder="Search drinks..."
-          className="flex-1 bg-transparent text-sm text-primary placeholder-surface outline-none"
-        />
+    <div className="flex flex-col gap-2 mb-3">
+      <div className='flex gap-2 items-center'>
+        {/* search input */}
+        <div className="flex items-center gap-2 border-2 border-surface focus-within:border-primary rounded-full px-4 py-2 w-full">
+            <RiSearchLine className="text-text font-bold" />
+            <input
+              value={searchValue}
+              onChange={e => setSearchValue(e.target.value)}
+              type="text"
+              placeholder="Search drinks..."
+              className="flex-1 bg-transparent text-sm text-primary placeholder-surface outline-none"
+            />
+          </div>
+          {isMobileFilterOpen ? <RiArrowUpSFill
+            className='sm:hidden text-primary'
+            size={28}
+            onClick={() => setIsMobileFilterOpen(!isMobileFilterOpen)}
+          />
+          : <RiFilterLine
+            className='sm:hidden text-primary'
+            size={28}
+            onClick={() => setIsMobileFilterOpen(!isMobileFilterOpen)}
+          />}
       </div>
+
       {/* filter */}
-      <div className="flex flex-col sm:flex-row items-start">
+      <div
+        className={`
+          flex flex-col sm:flex-row items-start
+          transition-all duration-300 ease-in-out
+          overflow-hidden transform
+          sm:max-h-none sm:opacity-100 sm:translate-y-0
+          ${isMobileFilterOpen ? 'max-h-96 opacity-100 translate-y-0' : 'max-h-0 opacity-0 -translate-y-4'}
+        `}
+      >
         <div className="w-full sm:w-64 md:w-80 lg:w-96 xl:w-[28rem] sm:mr-2 mb-2 sm:mb-0">
           <MultiSelect<SugarIceLabelType>
             options={iceOptions}
@@ -80,14 +106,8 @@ const FilterBar = ({
             placeholder="Toppings (any)"
           />
         </div>
-        {/* <p
-            onClick={() => {
-                setSelectedIce([]);
-                setSelectedSugar([]);
-            }}
-            className="underline cursor-pointer ml-0 sm:ml-2 mt-2 sm:mt-0"
-            >Clear Filters</p> */}
       </div>
+      
       {/* sort */}
       <div className="flex pl-2">
         <button

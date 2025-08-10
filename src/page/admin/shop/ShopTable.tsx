78f6ff063bdbc92985bc206ddac0326a40f0fa63
Eira <jie44011@gmail.com>
2025-08-10 19:27:11 +0100

    feat(admin-shop): add time sorting functionality to shop table

--- src/page/admin/shop/ShopTable.tsx
+++ src/page/admin/shop/ShopTable.tsx
@@ -1,8 +1,14 @@
+import { useState } from "react";
 import Tooltip from "../../../component/Tooltip";
 import { ShopType } from "../../../types/shop";
 import { IoShieldCheckmark } from "react-icons/io5";
 import { FaRegClock } from "react-icons/fa";
 import { MdEdit, MdDelete } from "react-icons/md";
+import { RxCaretSort } from 'react-icons/rx';
+import {
+  RiArrowDownSLine,
+  RiArrowUpSLine,
+} from 'react-icons/ri';
 import { shopColumns } from "../../../constants/shopColumnConfig";
 
 interface propsType {
@@ -15,6 +21,14 @@ interface propsType {
   getApproveInvalidKey: (shop: ShopType) => string[];
 }
 
+type SortKey = 'createdAt' | 'updatedAt' | '';
+type SortOrder = 'desc' | 'asc';
+type SortType = {
+  key: SortKey;
+  order: SortOrder;
+};
+
+
 const ShopTable = ({
     shops,
     visableLabelKeys,
@@ -24,21 +38,66 @@ const ShopTable = ({
     checkApproveValid,
     getApproveInvalidKey
 }: propsType) => {
+    const [sort, setSort] = useState<SortType>({
+      key: '',
+      order: 'desc'
+    });
+    const toggleSort = (key:SortKey) => {
+      setSort(preSort => {
+      if(key === preSort.key) {
+        if(preSort.order === 'desc') {
+            return {key, order: 'asc'}
+        } else {
+            // reset sort
+            return {key: '', order: 'desc'}
+        }
+      } else {
+          return {key, order: 'desc'}
+      }
+      })
+    };
+
+    const sortShops = sort.key ? shops.sort((a, b) => {
+        return sort.order === 'desc'
+        ? b.createdAt.localeCompare(a.createdAt)
+        : a.createdAt.localeCompare(b.createdAt);
+    }) : shops;
+
     return (
         <div className="overflow-x-auto rounded-xl shadow border border-gray-200">
             <table className="min-w-full divide-y divide-gray-200 bg-contrast text-sm text-center">
                 <thead className="bg-surface text-text-secondary font-semibold">
                 <tr>
-                    {shopColumns.filter(col => visableLabelKeys.includes(col.key)).map(col => (
-                        <th key={col.key} className="px-4 py-2">{ col.label }</th>
-                    ))}
+                    {shopColumns.filter(col => visableLabelKeys.includes(col.key)).map(col => {
+                        if(['createdAt', 'updatedAt'].includes(col.key)) {
+                            return (
+                                <th key={col.key} className="px-4 py-2">
+                                    <div
+                                        className="flex items-center cursor-pointer"
+                                        onClick={() => toggleSort(col.key as SortKey)}
+                                    >
+                                        { col.label }
+                                        {sort.key !== col.key ? (
+                                            <RxCaretSort  size={16}/>
+                                        ) : sort.order === 'desc' ? (
+                                            <RiArrowUpSLine/>
+                                        ) : (
+                                            <RiArrowDownSLine />
+                                        )}
+                                    </div>
+                                </th>
+                            )
+                        } else {
+                            return <th key={col.key} className="px-4 py-2">{ col.label }</th>
+                        }
+                    })}
                     <th className="px-4 py-2">Approve</th>
                     <th className="px-4 py-2">Edit</th>
                     <th className="px-4 py-2">Delete</th>
                 </tr>
                 </thead>
                 <tbody className="divide-y divide-gray-100">
-                    {shops.map((shop) => (
+                    {sortShops.map((shop) => (
                         <tr key={shop.id} className="hover:bg-background">
                             {shopColumns.filter(col => visableLabelKeys.includes(col.key)).map(col => {
                                 const val = shop[col.key as keyof ShopType];

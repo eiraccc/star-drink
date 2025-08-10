ee1a9302eb6a38494112fa94db83c9a94f4bc373
Eira <jie44011@gmail.com>
2025-08-10 17:06:15 +0100

    feat: load Google Analytics only in non-development environments

--- src/utils/firebase.ts
+++ src/utils/firebase.ts
@@ -17,4 +17,7 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
-const analytics = getAnalytics(app);
+
+if(import.meta.env.VITE_ENV !== "development") {
+  const analytics = getAnalytics(app);
+}

08d01a81fa8c251a3dcff33a5bd965844ae2e045
Eira <jie44011@gmail.com>
2025-08-11 21:44:29 +0100

    feat: add metadata with page title and favicon icon in app layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,18 @@
 import React from 'react';
+import type { Metadata } from 'next'
 import '../src/index.css';
 import Header from '../src/layout/Header';
 import Footer from '../src/layout/Footer';
 import { DrinkReviewProvider } from '../src/context/DrinkReviewContext';
 import { fetchReviews } from '../services/reviews';
+ 
+export const metadata: Metadata = {
+  title: 'Star Drink',
+  description: 'Enjoy your drinks!',
+  icons: {
+    icon: '/bubble-tea.png',
+  }
+}
 
 export default async function RootLayout({children}: {children: React.ReactNode}) {
     const reviewData = await fetchReviews();
